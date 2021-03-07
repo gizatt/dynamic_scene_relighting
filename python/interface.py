cbdc9c6f46d52a6149255feb1ded5796ba71208c
Greg Izatt <gizatt@mit.edu>
2021-03-07 03:20:00 +0000

    Reasonable demo settings to get away with

--- python/interface.py
+++ python/interface.py
@@ -1,22 +1,45 @@
+from functools import partial
 import consolemenu
 from consolemenu import ConsoleMenu
 from consolemenu.items import FunctionItem, SubmenuItem, CommandItem
+import logging
+
 
 class InterfaceManager():
     ''' Simple interface offering terminal control (since I'm running
     this over SSH right now) for RGB camera exposure + changing
     relighting modes. '''
 
-    def __init__(self):
+    def __init__(self, modes):
         self.menu = ConsoleMenu("Scene Relighting Control")
 
-        # Camera controls
+        self.refresh_item = FunctionItem("Refresh frame info", lambda: self.force_main_menu_refresh)
+        self.menu.append_item(self.refresh_item)
+
+        # Camera controls: exposure and depth range.
         self.camera_menu = ConsoleMenu("Camera Control")
         self.camera_menu_item = SubmenuItem("Change camera settings", self.camera_menu, menu=self.menu)
         self.set_camera_exposure = FunctionItem("Set exposure", self.update_exposure)
         self.camera_menu.append_item(self.set_camera_exposure)
         self.exposure = 0
+        self.depth_range = [0.9, 1.1]
+        self.set_min_depth_item = FunctionItem("Set min depth [%f]" % self.depth_range[0], self.set_min_depth)
+        self.set_max_depth_item = FunctionItem("Set max depth [%f]" % self.depth_range[1], self.set_max_depth)
+        self.camera_menu.append_item(self.set_min_depth_item)
+        self.camera_menu.append_item(self.set_max_depth_item)
         self.menu.append_item(self.camera_menu_item)
+
+        # Mode selection
+        self.modes = modes
+        self.mode_name = self.modes[0]
+        self.mode_menu = ConsoleMenu("Demo Modes", "Active mode: %s" % self.mode_name)
+        self.mode_menu_item = SubmenuItem("Change demo mode", self.mode_menu, menu=self.menu)
+        self.mode_items = []
+        for key in self.modes:
+            new_mode_item = FunctionItem("Set mode [%s]" % key, partial(self.set_mode, key))
+            self.mode_items.append(new_mode_item)
+            self.mode_menu.append_item(new_mode_item)
+        self.menu.append_item(self.mode_menu_item)
 
         # Enable/display meshcat vis
         self.meshcat_vis_active = True
@@ -24,12 +47,51 @@ class InterfaceManager():
         self.menu.append_item(self.toggle_meshcat_vis_item)
 
         # Adjust image save rate
-        self.image_save_rate = 30
+        self.image_save_rate = 0
         self.set_image_save_rate_item = FunctionItem(
             "Set frame divider for image saving [curr %d]" % self.image_save_rate,
             self.set_image_save_rate)
         self.menu.append_item(self.set_image_save_rate_item)
         self.menu.start()
+
+    def set_mode(self, key):
+        self.mode_name = key
+        self.mode_menu.subtitle = "Active mode: %s" % self.mode_name
+
+    def get_demo_mode(self):
+        return self.mode_name
+
+    def report_camera_stats(self, fps, min, max):
+        # Updates subtitle to report camera stats
+        self.menu.subtitle = "FPS [%.01f], Depth Range[%0.02f, %0.02f]" \
+                              % (fps, min, max)
+
+    def force_main_menu_refresh(self):
+        self.menu.screen.clear()
+        self.menu.draw()
+
+    def get_depth_range(self):
+        return self.depth_range
+
+    def set_min_depth(self):
+        new_depth = input("New min depth in meters:")
+        try:
+            new_depth = float(new_depth)
+            if new_depth >= 0 and new_depth <= 30.0:
+                self.depth_range[0] = new_depth
+                self.set_min_depth_item.text = "Set min depth [%f]" % new_depth
+        except ValueError():
+            pass
+
+    def set_max_depth(self):
+        new_depth = input("New max depth in meters:")
+        try:
+            new_depth = float(new_depth)
+            if new_depth >= 0 and new_depth <= 30.0:
+                self.depth_range[1] = new_depth
+                self.set_max_depth_item.text = "Set max depth [%f]" % new_depth
+        except ValueError():
+            pass
 
     def get_image_save_rate(self):
         return self.image_save_rate
--- python/run_depth_coloring_demo.py
+++ python/run_depth_coloring_demo.py
@@ -26,9 +26,17 @@ from calibration_utils import get_extrinsics, get_projector_intrinsics
 
 
 '''
-Reads from RGBD camera and spits out (colorized) depth to the projector.
+Reads from RGBD camera and spits out images to the projector.
 Reloads projector intr + extr calibrations from their respective
 files frequently so they can be hand-edited for fine tuning.
+
+Support demo modes:
+- Colorize by depth.
+- Colorize by normal.
+- Relight according to a rotating light.
+- Relight according to a fixed set of lights.
+
+Visualizes point cloud and lights (when present) to meshcat.
 
 Significant reference to
 https://github.com/IntelRealSense/librealsense/blob/development/wrappers/python/examples/pyglet_pointcloud_viewer.py
@@ -91,13 +99,80 @@ def meshcat_draw_pointcloud(vis, verts, colors):
         g.PointsMaterial(size=0.01)
     ))
 
-def meshcat_draw_lights(vis, light_locations):
+def meshcat_draw_lights(vis, light_locations, light_attenuations):
     N = light_locations.shape[1]
-    vis.set_object(g.Points(
-        g.PointsGeometry(
-            light_locations,
-            color=np.stack([np.array([1., 0.8, 0.7])]*N, axis=-1))
-    ))
+    colors = np.zeros((3, N))
+    colors[2, :] = light_attenuations
+    for k in range(N):
+        vis["lights"]["%d" % k].set_object(
+            g.Sphere(radius=0.05),
+            g.MeshLambertMaterial(
+                             color=0xffffff,
+                             reflectivity=0.8))
+        vis["lights"]["%d" % k].set_transform(
+            tf.translation_matrix(light_locations[:, k]))
+
+
+def colorize_depth(depth_image, min_depth, max_depth, cmap_name="hsv"):
+    return plt.get_cmap("hsv")((depth_image - min_depth)/(max_depth-min_depth))
+        
+def calc_normals(verts):
+    dy, dx = np.gradient(verts, axis=(0, 1))
+    n = np.cross(dx, dy)
+    norm = np.sqrt((n*n).sum(axis=2, keepdims=True))
+    normals = np.divide(n, norm, out=n, where=norm != 0)
+    normals = np.ascontiguousarray(normals)
+    return normals
+
+def calc_light_brightness(verts, light_locations, light_attenuations):
+    # Light locations should be a 3xN list of light locations.
+    # This should *really* be done in a shader, not by hand like this.
+    normals = calc_normals(verts)
+
+    N = light_locations.shape[1]
+    assert light_locations.shape[0] == 3, light_locations.shape
+    assert len(light_attenuations) == N
+
+    total_brightness = np.zeros(normals.shape[:2])
+
+    for k in range(N):
+        displacements = verts - light_locations[:, k]
+        distances = np.square(displacements).sum(axis=-1)
+        # Normalize displacements
+        norm_displacements = displacements / np.dstack([np.sqrt(distances)]*3)
+        alignments = np.clip((norm_displacements * normals).sum(axis=-1), 0., 1.)
+        brightness_diffuse = alignments/(1. + light_attenuations[k]*distances)
+        total_brightness += brightness_diffuse
+        if light_attenuations[k] > 0.:
+            # "Ambient" being much more generous about allowing light at 
+            # slanted normals
+            brightness_ambient = np.tanh(alignments*10.)/(light_attenuations[k]*distances)
+            total_brightness += brightness_ambient
+
+    return np.clip(total_brightness, 0., 1.)
+
+def generate_light_info(pattern_name, **kwargs):
+    if pattern_name == "orbiting single light":
+        xyz_center = kwargs["xyz_center"]
+        xyz_amplitude = kwargs["xyz_amplitude"]
+        a = kwargs["attenuation"]
+        light_location = xyz_center + np.array([
+            np.cos(time.time()),
+            np.sin(time.time()),
+            np.sin(time.time())]) * xyz_amplitude
+        light_locations = light_location.reshape(3, 1)
+        light_attenuations = [a]
+    elif pattern_name == "rembrandt lights":
+        # Key light up and to the one side (camera frame);
+        # fill light to the other, and weaker.
+        light_locations = np.array([
+            [-1., 1., 0.],
+            [1., 0., 0.]
+        ]).T
+        light_attenuations = [0.0, 1.]
+    else:
+        raise NotImplementedError("Rotating light pattern_name %s" % pattern_name)
+    return light_locations, light_attenuations
 
 if __name__ == "__main__":    
     logging.basicConfig(filename='../out/info.log', filemode='w', level=logging.DEBUG)
@@ -106,6 +181,7 @@ if __name__ == "__main__":
     # reminder for me to start a server.
     print("Opening meshcat server at default url: make sure meshcat-server is running.")
     vis = meshcat.Visualizer(zmq_url="tcp://127.0.0.1:6000")
+    vis.delete()
     camera_frame_vis = vis["camera_frame"]
     # Makes point cloud look "upright" in meshcat vis
     camera_frame_vis.set_transform(tf.rotation_matrix(np.pi/2, [1., 0., 0]))
@@ -119,7 +195,16 @@ if __name__ == "__main__":
         h=768
     )
 
-    interface = InterfaceManager()
+    modes = [
+        "Depth recoloring",
+        "Normal recoloring",
+        "Depth-colored rotating light",
+        "Orbiting hard light",
+        "Orbiting soft light",
+        "Moving light",
+        "Rembrandt lights"
+    ]
+    interface = InterfaceManager(modes=modes)
 
     # Subsample on depth image size
     sN = 4
@@ -133,21 +218,29 @@ if __name__ == "__main__":
         min_tracking_confidence=0.3)
 
     k = 0
+    last_time = time.time()
+    fps_est = 0.
     def on_idle(cls):
-        global k, camera_frame_vis
+        global k, camera_frame_vis, last_time, fps_est
         k += 1
+        now_time = time.time()
+        dt = now_time - last_time
+        last_time = now_time
+        fps_est = fps_est * 0.9 + (1. / dt)*0.1
 
         if not interface.is_alive():
             raise StopIteration
 
+        # Update camera config based on interface changes.
         realsense_manager.set_exposure(interface.get_exposure())
 
-        logging.info("Waiting for frame")
+        # Grab a new frame and process it.
         color_full, depth_image, points = realsense_manager.get_frame(include_pointcloud=True, do_alignment=False)
-        color_image = color_full[::sN, ::sN, ::-1]
+        color_image = color_full[::sN, ::sN, :]
         h, w = color_image.shape[:2]
         verts = np.asarray(points.get_vertices(2)).reshape(h, w, 3)
 
+        # Do drawing and saving as requested.
         if interface.get_meshcat_vis_active():
             meshcat_draw_pointcloud(camera_frame_vis, verts, color_image)
         
@@ -156,38 +249,74 @@ if __name__ == "__main__":
             logging.info("Sending downsampled pts to meshcat and saving color image")
             plt.imsave("../out/curr_color.png", color_full[::-1, ::-1, ::-1])
             #cv2.imwrite("../out/curr_depth.png" % k, depth_image[::-1, ::-1].astype(np.uint16))
-        logging.info("Depth range:[%f, %f]", np.max(depth_image)/1000., np.mean(depth_image)/1000.)
-        min_depth = 0.90*1000
-        max_depth = 1.1*1000
         
-        depth_color_source = plt.get_cmap("hsv")((depth_image - min_depth)/(max_depth-min_depth))
-        #depth_color_source = np.uint8(depth_color_source * 255)
-        color_source = depth_color_source
+        interface.report_camera_stats(
+            fps=fps_est,
+            min=np.min(depth_image)/1000.,
+            max=np.max(depth_image)/1000.)
 
-        # Very inefficient normal estimation
-        if True:
-            dy, dx = np.gradient(verts, axis=(0, 1))
-            n = np.cross(dx, dy)
-            norm = np.sqrt((n*n).sum(axis=2, keepdims=True))
-            normals = np.divide(n, norm, out=n, where=norm != 0)
-            normals = np.ascontiguousarray(normals)
-            #normal_image = np.uint8(normals * 127 + 127)
-            plt.imsave("../out/curr_normals.png", normals[::1, ::-1, :]/2. + 0.5)
+        depth_range = interface.get_depth_range()
+        min_depth = depth_range[0]*1000
+        max_depth = depth_range[1]*1000
 
-            # Simulate a light orbiting around the z axis by brighting
-            # base on dot product of a time-varying normal and the
-            # normal image.
-            t = k * np.pi/15
-            logging.info("T: %f", t)
-            light_direction = np.array([2.*np.cos(t), 2.*np.sin(t), 1.0])
-            light_direction /= np.linalg.norm(light_direction)
-            normal_alignment = (light_direction * normals).sum(axis=-1)
-            brightness = np.tanh(normal_alignment*2. - 1.)*0.5 + 0.5
-            brightness[depth_image < min_depth] = 0.
-            brightness[depth_image > max_depth] = 0.
+        # Finally, branch by demo mode.
+        mode_name = interface.get_demo_mode()
+        light_locations = None
+        if mode_name == "Depth recoloring":
+            color_source = colorize_depth(depth_image, min_depth, max_depth)
+        elif mode_name == "Normal recoloring":
+            normals = calc_normals(verts)
+            color_source = np.ones(normals.shape[:2] + (4,))
+            color_source[:, :, :3] = normals*0.5 + 0.5
+        elif mode_name == "Depth-colored rotating light":
+            color_source = colorize_depth(depth_image, min_depth, max_depth)
+            light_locations, light_attenuations = generate_light_info(
+                pattern_name="orbiting single light",
+                xyz_center=np.array([0., 0., 0.7]),
+                xyz_amplitude=np.array([1.0, 1.0, 0.0]),
+                attenuation=0.0)
+            brightness = calc_light_brightness(
+                verts, light_locations, light_attenuations)
+            brightness *= np.logical_and(
+                depth_image >= min_depth, depth_image <= max_depth)
+            color_source[:, :, -1] = brightness
+        elif mode_name == "Orbiting hard light":
+            light_locations, light_attenuations = generate_light_info(
+                pattern_name="orbiting single light",
+                xyz_center=np.array([0., 0., 0.7]),
+                xyz_amplitude=np.array([0.3, 0.3, 0.]),
+                attenuation=0.0)
+            brightness = calc_light_brightness(verts, light_locations, light_attenuations)
+            color_source = np.dstack([brightness]*4)
+        elif mode_name == "Orbiting soft light":
+            light_locations, light_attenuations = generate_light_info(
+                pattern_name="orbiting single light",
+                xyz_center=np.array([0., 0., 0.7]),
+                xyz_amplitude=np.array([0.3, 0.3, 0.]),
+                attenuation=10.0)
+            brightness = calc_light_brightness(verts, light_locations, light_attenuations)
+            color_source = np.dstack([brightness]*4)
+        elif mode_name == "Moving light":
+            light_locations, light_attenuations = generate_light_info(
+                pattern_name="orbiting single light",
+                xyz_center=np.array([0., 0., 0.9]),
+                xyz_amplitude=np.array([0.0, 0.0, 0.3]),
+                attenuation=25.0)
+            brightness = calc_light_brightness(verts, light_locations, light_attenuations)
+            color_source = np.dstack([brightness]*4)
+        elif mode_name == "Rembrandt lights":
+            light_locations, light_attenuations = generate_light_info(
+                pattern_name="rembrandt lights")
+            brightness = calc_light_brightness(verts, light_locations, light_attenuations)
+            color_source = np.dstack([brightness]*4)
+        else:
+            logging.error("Bad mode name: %s" % mode_name)
+            return
 
-            #brightness = np.repeat(brightness[:, :, np.newaxis], 4, axis=-1)
-            #brightness_color_source = np.uint8(brightness * 255)
+        if light_locations is not None:
+            meshcat_draw_lights(camera_frame_vis, light_locations, light_attenuations)
+        else:
+            camera_frame_vis["lights"].delete()
 
         results = face_mesh_detector.process(color_image)
         if results.multi_face_landmarks:
@@ -205,11 +334,6 @@ if __name__ == "__main__":
                     landmark_pixel_locations.append(landmark_px)
                 logging.info("Pixel locations: %s", str(landmark_pixel_locations))
 
-        #plt.imsave("out/curr_depth.png", depth_color_source[::-1, ::-1, ::-1])
-        color_source =  depth_color_source
-        #color_source = np.ones(color_image.shape[:2] + (4,))
-        color_source[:, :, :-1] = normals*0.5 + 0.5
-        color_source[:, :, -1] = brightness
         #color_source = depth_color_source
         # copy image data to pyglet
         cls.update_geometry(verts, color_source)
