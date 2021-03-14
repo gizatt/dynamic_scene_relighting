9546dfc87e32a8506879834e4d976d71595c3d15
Greg Izatt <gizatt@mit.edu>
2021-03-14 21:14:02 +0000

    Remove c code to clean up

--- src/shader.h
+++
@@ -1,6 +0,0 @@
-#include <iostream>             // for cout
-#include <fstream>
-#include <GL/glew.h>     // has to be included before gl.h, or any header that includes gl.h
-#include <GL/freeglut.h>
-
-GLuint LoadShader(const char *vertex_path, const char *fragment_path);
